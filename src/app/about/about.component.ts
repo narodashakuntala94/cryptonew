e60e1ecdbc0362e9fc83a5d533cfbcf0e2d0edf4
Shauntala Naroda <narodashakuntala94@gmail.com>
2019-04-02 10:22:54 +0530

    upload project

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+  
+
+    // text1='thuuuhijhoi'
+    // MyFunction(){
+    //  this.text1=text1;
+
+    // }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  //  Myname='My name is sakhu'
+
+  //  text='input text changed'
+  text=''
+ 
+  // callMyfunction(){
+  //   // console.log('button clicked');
+  //   alert('button clicked');
+  // }
+  //   updatevent(e){
+  //   this.text=e.target.value;
+  // }
+  ngOnInit() {
+  }
+
+}
