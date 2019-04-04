87d9a28b6ae042ee80482af8d9ebd5c7854b2925
sourabh sahu <Sourabh.Sahu@accolite.com>
2019-04-04 12:40:30 +0530

    changes for menu

--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+declare var $:any;
 
 @Component({
   selector: 'app-exchange',
   templateUrl: './exchange.component.html',
-  styleUrls: ['./exchange.component.css']
+  styleUrls: ['./exchange.component.css'],
+  encapsulation:ViewEncapsulation.None
 })
 export class ExchangeComponent implements OnInit {
-
-  constructor() { }
+  hideMenu:boolean = false;
+  constructor()  { }
 
   ngOnInit() {
+  
+
+  }
+
+  ngAfterViewInit(){
+    // this.toggle()
+  }
+
+  toggle(){
+    
+    this.hideMenu = !this.hideMenu
+    event.stopPropagation();
+
+//     $('#menu-button').click(function(e){
+//       event.stopPropagation();
+//        $('#hide-menu').toggleClass('show-menu');
+//       });
+//   $('#hide-menu').click(function(e){
+//       event.stopPropagation();
+//       });
+//   $('body,html').click(function(e){
+//          $('#hide-menu').removeClass('show-menu');
+//       }); 
+//       $('#menu-button1').click(function(e){
+//       e.stopPropagation();
+//        $('#hide-menu').toggleClass('show-menu');
+//       });
+      
+      
+// $('.dropdown-menu a').click(function(){
+// $('#selected').text($(this).text());
+// });
+
   }
 
 }
