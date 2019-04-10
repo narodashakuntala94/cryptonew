5e69921662a654937a44649309e17133887a9f22
sourabh sahu <Sourabh.Sahu@accolite.com>
2019-04-10 12:21:24 +0530

    changes commied for admin

--- src/app/adminfooter/adminfooter.component.ts
+++ src/app/adminfooter/adminfooter.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-adminfooter',
   templateUrl: './adminfooter.component.html',
-  styleUrls: ['./adminfooter.component.css']
+  styleUrls: ['./adminfooter.component.css'],
+  encapsulation:ViewEncapsulation.None
 })
 export class AdminfooterComponent implements OnInit {
 
--- src/app/adminheader/adminheader.component.ts
+++ src/app/adminheader/adminheader.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-adminheader',
   templateUrl: './adminheader.component.html',
-  styleUrls: ['./adminheader.component.css']
+  styleUrls: ['./adminheader.component.css'],
+  encapsulation:ViewEncapsulation.None
 })
 export class AdminheaderComponent implements OnInit {
 
